fa9db0f726ab26fc42b7b05ddf6509ab9982e64e
v0agent <it+v0agent@vercel.com>
2026-05-29 18:59:20 +0000

    feat: adjust hero section height to account for header

--- components/hero.tsx
+++ components/hero.tsx
@@ -46,7 +46,7 @@ export function Hero() {
   )
 
   return (
-    <section className="w-full h-[100dvh] flex flex-col">
+    <section className="w-full h-[calc(100dvh-116px)] flex flex-col">
       {/* Image Carousel - takes remaining space */}
       <div className="overflow-hidden touch-pan-y flex-1 min-h-0" ref={emblaRef}>
         <div className="flex will-change-transform h-full">
